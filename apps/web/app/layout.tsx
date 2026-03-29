76129b198ff7a38cf57b7ee601e934dece17dcd1
Fris El <uxfris@gmail.com>
2026-03-29 03:09:25 -0400

    feat: integrate TooltipProvider into RootLayout for enhanced UI tooltips

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@workspace/ui/lib/utils";
+import { TooltipProvider } from "@workspace/ui/components/tooltip";
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -26,7 +27,8 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", fontSans.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <TooltipProvider>{children}</TooltipProvider></ThemeProvider>
       </body>
     </html>
   )
