d7e7520771749ded6de87949243d37fda94b1d4a
Fris El <uxfris@gmail.com>
2026-04-03 05:20:35 -0400

    refactor: replace mobile-sidebar-toggle with mobile-sidebar-header to support sidebar state toggling

--- apps/web/app/(app)/dashboard/_components/sidebar/mobile-sidebar-header.tsx
+++ apps/web/app/(app)/dashboard/_components/sidebar/mobile-sidebar-header.tsx
@@ -6,11 +6,15 @@ import { HamburgerMenu } from "@solar-icons/react";
 import { Button } from "@workspace/ui/components/button";
 import { DropdownUserMenu } from "./nav-dropdown-user-menu";
 import Link from "next/link";
+import { useSidebar } from "@workspace/ui/components/sidebar";
 
 export function MobileHeader() {
+
+    const { toggleSidebar } = useSidebar()
+
     return (
-        <div className="md:hidden flex items-center justify-between py-2">
-            <Button variant="ghost" size="icon"><HamburgerMenu className="size-6" /></Button>
+        <div className="md:hidden flex items-center justify-between py-2 bg-background">
+            <Button variant="ghost" size="icon" onClick={() => toggleSidebar()}><HamburgerMenu className="size-6" /></Button>
             <Link href="/" className="flex items-center gap-1">
                 <span className="w-8">
                     <LogoIcon />
--- apps/web/app/(app)/dashboard/layout.tsx
+++ apps/web/app/(app)/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import { AppSidebar } from "./_components/sidebar/app-sidebar";
 import { getPlatform } from "@workspace/ui/lib/platform";
 import { PlatformProvider } from "@workspace/ui/components/platform-provider";
 import { ShortcutProvider } from "@workspace/ui/components/shortcut-provider";
-import { MobileHeader } from "./_components/sidebar/mobile-sidebar-toggle";
+import { MobileHeader } from "./_components/sidebar/mobile-sidebar-header";
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
     const platform = await getPlatform();
