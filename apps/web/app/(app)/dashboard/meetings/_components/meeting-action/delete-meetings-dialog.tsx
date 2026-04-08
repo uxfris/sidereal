15c1f91da2fd73670227faeafb0e4234a805d45c
Fris El <uxfris@gmail.com>
2026-04-08 00:18:49 -0400

    feat: implement meeting deletion using new alert dialog component

---
+++ apps/web/app/(app)/dashboard/meetings/_components/meeting-action/delete-meetings-dialog.tsx
@@ -0,0 +1,61 @@
+import { TrashBin2 } from "@solar-icons/react";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@workspace/ui/components/alert-dialog";
+import { Button } from "@workspace/ui/components/button";
+import { Input } from "@workspace/ui/components/input";
+
+
+const MOCK_MEETINGS = [
+    {
+        id: "1",
+        name: "Product Strategy",
+    },
+    {
+        id: "2",
+        name: "Design Systems",
+    },
+    {
+        id: "3",
+        name: "Marketing",
+    }
+]
+
+export function DeleteMeetingsDialog() {
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button size="xs" variant="destructive" className="bg-transparent">
+                    <TrashBin2 />
+                    Delete
+                </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>
+                        Delete Meetings
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                        Are you sure you want to delete these meetings? This action cannot be undone
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <div className="px-4 max-h-64 overflow-y-auto">
+                    {MOCK_MEETINGS.map((meeting) =>
+                        <li key={meeting.id} className="text-sm font-medium text-muted-foreground">
+                            {meeting.name}
+                        </li>)}
+                </div>
+                <div className="space-y-2">
+                    <p className="text-sm to-muted-foreground">To confirm deletion, type <span className="font-semibold">DELETE {MOCK_MEETINGS.length}</span> below:</p>
+                    <Input autoFocus placeholder={`Delete ${MOCK_MEETINGS.length}`} />
+                </div>
+                <AlertDialogFooter>
+                    <AlertDialogCancel variant="ghost">
+                        Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction variant="destructive">
+                        Delete
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
