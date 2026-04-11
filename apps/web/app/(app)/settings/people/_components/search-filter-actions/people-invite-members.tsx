e416901101776fc8fdcdbbfaddac6106d63eae4a
Fris El <uxfris@gmail.com>
2026-04-11 05:04:03 -0400

    feat: implement invite member and link generation dialogs and update label component styles

--- apps/web/app/(app)/settings/people/_components/search-filter-actions/people-invite-members.tsx
+++ apps/web/app/(app)/settings/people/_components/search-filter-actions/people-invite-members.tsx
@@ -1,11 +1,58 @@
-import { UserPlus } from "@solar-icons/react/ssr";
+"use client"
+
+import { UserPlus } from "@solar-icons/react";
 import { Button } from "@workspace/ui/components/button";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@workspace/ui/components/dialog";
+import { Field, FieldLabel } from "@workspace/ui/components/field";
+import { Input } from "@workspace/ui/components/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@workspace/ui/components/select";
 
 export function PeopleInviteMembers() {
     return (
-        <Button size="xs">
-            <UserPlus />
-            Invite members
-        </Button>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button size="xs">
+                    <UserPlus />
+                    Invite members
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Invite members
+                    </DialogTitle>
+                    <DialogDescription>
+                        Invite members to your workspace by email
+                    </DialogDescription>
+                </DialogHeader>
+                <form className="space-y-4">
+                    <Field>
+                        <FieldLabel>Email</FieldLabel>
+                        <Input placeholder="example1@example.com, example2@example.com" />
+                    </Field>
+                    <Field>
+                        <FieldLabel>Role</FieldLabel>
+                        <Select>
+                            <SelectTrigger>
+                                <SelectValue placeholder="choose role" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectItem value="admin">Admin</SelectItem>
+                                    <SelectItem value="member">Member</SelectItem>
+                                    <SelectItem value="guest">Guest</SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </Field>
+                </form>
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button variant="ghost">Cancel</Button>
+                    </DialogClose>
+                    <Button>Invite</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog >
     )
 }
--- apps/web/app/(app)/settings/people/_components/search-filter-actions/people-link-invite.tsx
+++ apps/web/app/(app)/settings/people/_components/search-filter-actions/people-link-invite.tsx
@@ -1,11 +1,99 @@
-import { Link } from "@solar-icons/react/ssr";
+"use client"
+
+import { ClockCircle, Link, Refresh, TrashBin2 } from "@solar-icons/react";
 import { Button } from "@workspace/ui/components/button";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@workspace/ui/components/dialog";
+import { Field, FieldLabel } from "@workspace/ui/components/field";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@workspace/ui/components/select";
+import { Copy } from "lucide-react";
+import { useState } from "react";
 
 export function PeopleLinkInvite() {
+
+    const [isSuccess, setIsSuccess] = useState(false)
+
+
+    const createLink = () => {
+        setIsSuccess(true)
+    }
+
     return (
-        <Button size="xs" variant="secondary">
-            <Link />
-            Invite link
-        </Button>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button size="xs" variant="secondary">
+                    <Link />
+                    Invite link
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Workspace invite link
+                    </DialogTitle>
+                    <DialogDescription>
+                        Generate a link to invite people to this workspace.
+                        Link expire after 5 days.
+                    </DialogDescription>
+                </DialogHeader>
+                <form>
+                    <Field>
+                        <FieldLabel>Role</FieldLabel>
+                        <Select>
+                            <SelectTrigger>
+                                <SelectValue placeholder="choose role" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectItem value="admin">Admin</SelectItem>
+                                    <SelectItem value="member">Member</SelectItem>
+                                    <SelectItem value="guest">Guest</SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </Field>
+                </form>
+                {isSuccess && <>
+                    <div>
+                        <div className="space-y-2">
+                            <p className="text-sm text-foreground font-semibold">
+                                Invite link
+                            </p>
+                            <Button className="w-full">
+                                <Copy />
+                                Copy
+                            </Button>
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center gap-1 text-sm text-muted-foreground-2">
+                                <ClockCircle />
+                                Expires Apr 16, 4:51 AM
+                            </div>
+                            <Button variant="ghost" className="p-0 bg-transparent hover:bg-transparent">
+                                <Refresh />
+                                Regenerate
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <Button variant="ghost" className="text-destructive hover:text-destructive">
+                            <TrashBin2 />
+                            Delete
+                        </Button>
+                        <DialogClose asChild>
+                            <Button variant="outline">Close</Button>
+
+                        </DialogClose>
+
+                    </div>
+                </>}
+
+                {!isSuccess && <DialogFooter>
+                    <Button onClick={createLink}>Create invite link</Button>
+                    <DialogClose asChild>
+                        <Button variant="ghost">Cancel</Button>
+                    </DialogClose>
+                </DialogFooter>}
+            </DialogContent>
+        </Dialog >
     )
 }
