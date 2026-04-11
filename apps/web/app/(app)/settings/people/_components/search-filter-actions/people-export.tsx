07a8b4a3397a0eaf565f08bef5e8836177fbb2ac
Fris El <uxfris@gmail.com>
2026-04-11 05:06:59 -0400

    feat: implement CSV export toast notification

--- apps/web/app/(app)/settings/people/_components/search-filter-actions/people-export.tsx
+++ apps/web/app/(app)/settings/people/_components/search-filter-actions/people-export.tsx
@@ -1,9 +1,17 @@
-import { DownloadMinimalistic } from "@solar-icons/react/ssr";
+"use client"
+
+import { DownloadMinimalistic } from "@solar-icons/react";
 import { Button } from "@workspace/ui/components/button";
+import { toast } from "sonner";
 
 export function PeopleExport() {
+
+    const exportCSV = () => {
+        toast.success("Export ready. Download should begin shortly")
+    }
+
     return (
-        <Button size="xs" variant="secondary">
+        <Button size="xs" variant="secondary" onClick={exportCSV}>
             <DownloadMinimalistic />
             Export
         </Button>
